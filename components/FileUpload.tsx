1dc57ea2c83aeaf9e033df20e0f5434f00f85e09
Rishabh <rishabhgoel2020@gmail.com>
2025-05-11 15:48:26 +0530

    AWS S3 Bucket implement files upload on s3

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
@@ -7,8 +8,21 @@ const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
     accept : {'application/pdf' : [".pdf"]},
     maxFiles : 1,
-    onDrop : (acceptedFiles) => {
+    onDrop : async (acceptedFiles) => {
         console.log("file accepted : ",acceptedFiles); 
+        const file = acceptedFiles[0];
+
+        if(file.size > 10*1024*1024){
+          alert("Upload smaller file then 10MB")
+        }
+
+        try {
+          const data = await uploadToS3(file)
+          console.log("data : ", data);
+          
+        } catch (error) {
+          console.log("Error in fileUpload : ", error)
+        }
     }
   });
   return (
