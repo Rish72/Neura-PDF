043a64d7a2e7175eeda3874c71c303521ea519e3
Rishabh <rishabhgoel2020@gmail.com>
2025-06-10 14:10:39 +0530

    Add animation to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,16 +2,16 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import {motion} from "motion/react"
 export default function NavBar() {
   return (
-    <nav className="absolute w-full p-3 flex justify-between items-center text-white overflow-hidden">
+    <motion.nav initial={{y : -100}} animate={{y : 0}} transition={{delay : 0.4, duration: 0.6}}  className="absolute w-full p-3 flex justify-between items-center text-white overflow-hidden">
       <div className="cursor-pointer">
         <Link className="flex items-center gap-4" href="/">
         <Image src="/logo2.ico" width={35} height={35} alt="Neura AI" />
         <h2 className="font-semibold text-lg text-black">Neura Ai</h2>
       </Link>
       </div>
-    </nav>
+    </motion.nav>
   );
 }
